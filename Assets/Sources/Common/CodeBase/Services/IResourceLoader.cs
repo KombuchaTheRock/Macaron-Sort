7b1f200031bc08b998b4b0c70218556f54c9eff1
Kombucha <dantedelefrutt@gmail.com>
2025-09-10 16:04:15 +0700

    Release ResourceLoader

---
+++ Assets/Sources/Common/CodeBase/Services/IResourceLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Sources.Common.CodeBase.Services
+{
+    public interface IResourceLoader
+    {
+        T LoadAsset<T>(string path) where T : Object;
+        T[] LoadAllAssets<T>(string path) where T : Object;
+    }
+}
