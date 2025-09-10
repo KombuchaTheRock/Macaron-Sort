fd148be61df6559a05b51a0cd8fe6150917c4715
Kombucha <dantedelefrutt@gmail.com>
2025-09-10 20:00:37 +0700

    Refactoring

--- Assets/Sources/Features/HexagonSort/Scripts/StackGenerator.cs
+++ Assets/Sources/Features/HexagonSort/Scripts/StackGenerator.cs
@@ -1,68 +1,72 @@
 using System.Collections.Generic;
-using System.Linq;
 using Sources.Common.CodeBase.Services;
 using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 namespace Sources.Features.HexagonSort.Scripts
 {
     public class StackGenerator : MonoBehaviour
     {
-        [SerializeField] private Transform _stackPositionsParent;
-        [SerializeField] private float _hexagonHeight; //Config
-        [SerializeField] private int _minStackSize; //Config
-        [SerializeField] private int _maxStackSize; //Config
-        [Space(5), SerializeField] private Color[] _colors; //Config
+        private float _hexagonHeight;
+        private int _minStackSize;
+        private int _maxStackSize;
+        private Color[] _colors;
+        private List<Vector3> _spawnPositions;
 
         private IGameFactory _factory;
 
         [Inject]
-        public void Construct(IGameFactory gameFactory)
-        {
+        public void Construct(IGameFactory gameFactory) =>
             _factory = gameFactory;
+
+        public void Initialize(HexagonStackConfig stackConfig, List<Vector3> spawnPositions)
+        {
+            _spawnPositions = spawnPositions;
+            _hexagonHeight = stackConfig.HexagonHeight;
+            _minStackSize = stackConfig.MinStackSize;
+            _maxStackSize = stackConfig.MaxStackSize;
+            _colors = stackConfig.Colors;
         }
 
-        private void Start() => //Вынести в LoadLevelState
-            GenerateStacks();
-
-        private void GenerateStacks()
+        public void GenerateStacks()
         {
-            for (int i = 0; i < _stackPositionsParent.childCount; i++)
-                GenerateStack(_stackPositionsParent.GetChild(i));
+            foreach (Vector3 position in _spawnPositions)
+                GenerateStack(position);
         }
 
-        private void GenerateStack(Transform parent)
+        private void GenerateStack(Vector3 spawnPosition)
         {
-            GameObject hexagonStack = _factory.CreateHexagonStack(parent.position, parent);
-            hexagonStack.name = $"Stack {parent.GetSiblingIndex()}";
+            GameObject hexagonStack = _factory.CreateHexagonStack(spawnPosition, transform);
+            hexagonStack.name = $"Stack {transform.GetSiblingIndex()}";
 
             int amount = Random.Range(_minStackSize, _maxStackSize); //Вынести в фабрику
-            int firstColorHexagonAmount = Random.Range(0, amount);
 
             Color[] randomColors = GetRandomColors();
-
-            for (int i = 0; i < amount; i++) 
-                SpawnHexagon(i, parent, hexagonStack, firstColorHexagonAmount, randomColors);
+            int firstColorIndex = Random.Range(0, amount);
+            
+            for (int i = 0; i < amount; i++)
+                SpawnHexagon(i, hexagonStack, randomColors, firstColorIndex);
         }
 
-        private void SpawnHexagon(int index, Transform parent, GameObject hexagonStack, int firstColorHexagonAmount,
-            Color[] randomColors)
+        private void SpawnHexagon(int index, GameObject hexagonStack,
+            Color[] randomColors, int firstColorIndex)
         {
-            Vector3 hexagonLocalPosition = Vector3.up * index * _hexagonHeight; 
+            Vector3 hexagonLocalPosition = Vector3.up * index * _hexagonHeight;
             Vector3 spawnPosition = hexagonStack.transform.TransformPoint(hexagonLocalPosition);
 
-            Hexagon hexagon = _factory.CreateHexagon(spawnPosition, parent);
-            Color color = index <= firstColorHexagonAmount ? randomColors[0] : randomColors[1];
-            
+            Hexagon hexagon = _factory.CreateHexagon(spawnPosition, hexagonStack.transform);
+
+            Color color = index <= firstColorIndex ? randomColors[0] : randomColors[1];
             hexagon.SetColor(color);
         }
 
         private Color[] GetRandomColors()
         {
-            Color firstColor = _colors.OrderBy(_ => Random.value).First();
-            Color secondColor = _colors.OrderBy(_ => Random.value).First();
-
-            return new[] { firstColor, secondColor };
+           Color firstColor = _colors[Random.Range(0, _colors.Length)];
+           Color secondColor = _colors[Random.Range(0, _colors.Length)];
+           
+           return new[] { firstColor, secondColor };
         }
     }
 }
